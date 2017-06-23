6c5b98109140fc70da0563e4dc962b8c00c93366
zhongxinzhi <1123858432@qq.com>
2017-06-23 16:32:56 +0800

    云音乐 slider滑块

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <main>
       <router-view></router-view>
     </main>
+    <m-slider></m-slider>
   </div>
 </template>
 <script>
   import 'css/common'
   // 导入字体
   import 'lib/font-awesome/less/font-awesome'
+  // 导入组件
   import MHeader from 'components/header'
+  import MSlider from 'components/Slider'
   export default {
     name: 'app',
-    components: { MHeader }
+    components: { MHeader, MSlider }
   }
 </script>
 <style lang="less">
---
+++ src/components/Slider.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="slider">
+    <div class="slider-content" @click.stop>
+      <div class="user-info">
+        <template>
+          <div class="avatar"></div>
+            <div class="info">
+              <span class="name">zhongxinzhi</span>
+              <span class="level">lv.7</span>
+              <a class="sign">签到</a>
+            </div>
+        </template>
+      </div>
+      <ul>
+        <li v-for="item in sliderList">
+          <a href="javascript:void(0)" class="slider-item">
+            <div class="left">
+              <i :class="['fa',item.icon]"></i> <span>{{ item.title }}</span>
+            </div>
+            <div class="right" v-if="item.right">{{ item.right }}</div>
+          </a>
+        </li>
+      </ul>
+      <div class="tools">
+        <ul>
+          <li><a href="javascript:void(0)">夜间模式</a></li>
+          <li><a href="javascript:void(0)">设置</a></li>
+          <li><a href="javascript:void(0)">退出</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+  @import "../assets/css/variables";
+  @import "../assets/css/mixins";
+  .slider{
+    position: fixed;
+    right: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    background: @modal-color;
+  }
+  .slider-content{
+    background: #ffffff;
+    overflow: auto;
+    width: 350px;
+    height: 100%;
+    position: relative;
+    .user-info{
+      height: 300px;
+      background: fade(#000,80%);
+      color: #ffffff;
+      padding: 130px 10px 0;
+      .avatar{
+        height: 100px;
+        width: 100px;
+        margin-bottom: 10px;
+        border-radius: 50%;
+        border: 1px solid #ffffff;
+      }
+      .info{
+        .clearfix()
+      }
+      .level,.sign{
+        border: 1px solid #fff;
+        padding: 0 10px;
+        font-size: 12px;
+        border-radius: 8px;
+      }
+      .sign{
+        float: right;
+        line-height: 18px;
+      }
+    }
+    .slider-item{
+      display: flex;
+      padding: 0 10px;
+      border-bottom: 1px solid fade(#ccc,50%);
+      justify-content: space-between;
+      .left,.right{
+        height: 40px;
+        line-height: 40px;
+      }
+      .right{
+        font-size: 12px;
+      }
+    }
+  }
+  .tools{
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    ul{
+      display: flex;
+      li{
+        flex: 1;
+        a{
+          display: block;
+          height: 40px;
+          text-align: center;
+          line-height: 40px;
+        }
+      }
+    }
+  }
+</style>
+<script>
+  export default {
+    data() {
+      return {
+        sliderList: [
+          {
+            icon: 'fa-paper-plane-o',
+            title: '我的信息',
+            right: '网易云音乐'
+          },
+          {
+            icon: 'fa-paper-plane-o',
+            title: '我的信息',
+            right: '网易云音乐'
+          },
+          {
+            icon: 'fa-paper-plane-o',
+            title: '我的信息',
+            right: '网易云音乐'
+          },
+          {
+            icon: 'fa-paper-plane-o',
+            title: '我的信息',
+            right: '网易云音乐'
+          },
+          {
+            icon: 'fa-paper-plane-o',
+            title: '我的信息',
+            right: '网易云音乐'
+          },
+          {
+            icon: 'fa-paper-plane-o',
+            title: '我的信息',
+            right: '网易云音乐'
+          },
+          {
+            icon: 'fa-paper-plane-o',
+            title: '我的信息',
+            right: '网易云音乐'
+          }
+        ]
+      }
+    },
+    methods: {},
+    created() {
+    },
+    mounted() {
+    }
+  }
+</script>
